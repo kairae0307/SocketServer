123f0bd5e760b55c0ab2c478e624a8b0b1a93e2a
Daniel Kye <kairae@naver.com>
2023-12-14 21:09:26 +0900

    first commit

---
+++ SocketServer/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SocketServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Client_label = new System.Windows.Forms.Label();
+            this.label32 = new System.Windows.Forms.Label();
+            this.spTimer = new System.Timers.Timer();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSendMsg = new System.Windows.Forms.Button();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.spTimer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Client_label
+            // 
+            this.Client_label.AutoSize = true;
+            this.Client_label.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.Client_label.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Client_label.ForeColor = System.Drawing.Color.Black;
+            this.Client_label.Location = new System.Drawing.Point(213, 28);
+            this.Client_label.Name = "Client_label";
+            this.Client_label.Size = new System.Drawing.Size(113, 16);
+            this.Client_label.TabIndex = 75;
+            this.Client_label.Text = "Client Status";
+            // 
+            // label32
+            // 
+            this.label32.AutoSize = true;
+            this.label32.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label32.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label32.ForeColor = System.Drawing.Color.Black;
+            this.label32.Location = new System.Drawing.Point(33, 27);
+            this.label32.Name = "label32";
+            this.label32.Size = new System.Drawing.Size(119, 16);
+            this.label32.TabIndex = 74;
+            this.label32.Text = "Server Status";
+            // 
+            // spTimer
+            // 
+            this.spTimer.Enabled = true;
+            this.spTimer.Interval = 1000D;
+            this.spTimer.SynchronizingObject = this;
+            this.spTimer.Elapsed += new System.Timers.ElapsedEventHandler(this.spTimer_Elapsed);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 66);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(540, 302);
+            this.richTextBox1.TabIndex = 76;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSendMsg
+            // 
+            this.btnSendMsg.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSendMsg.Location = new System.Drawing.Point(489, 395);
+            this.btnSendMsg.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnSendMsg.Name = "btnSendMsg";
+            this.btnSendMsg.Size = new System.Drawing.Size(78, 25);
+            this.btnSendMsg.TabIndex = 78;
+            this.btnSendMsg.Text = "SEND";
+            this.btnSendMsg.UseVisualStyleBackColor = true;
+            this.btnSendMsg.Click += new System.EventHandler(this.btnSendMsg_Click);
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.Location = new System.Drawing.Point(27, 395);
+            this.txtSendMsg.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.Size = new System.Drawing.Size(451, 21);
+            this.txtSendMsg.TabIndex = 77;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(489, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 25);
+            this.button1.TabIndex = 79;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 441);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSendMsg);
+            this.Controls.Add(this.txtSendMsg);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Client_label);
+            this.Controls.Add(this.label32);
+            this.Name = "Form1";
+            this.Text = "SERVER";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.spTimer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label Client_label;
+        public System.Windows.Forms.Label label32;
+        private System.Timers.Timer spTimer;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSendMsg;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SocketServer/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using System.IO.Ports;
+using System.Net;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Data.SqlClient;
+using System.Configuration;
+//using Excel = Microsoft.Office.Interop.Excel;
+namespace SocketServer
+{
+    public partial class Form1 : Form
+    {
+
+        public bool ServerOpen = false;
+        private Socket ServerSocket = null;
+        private Thread ServerThread = null;
+
+        public string TACHO2_path = "";
+        public Socket TCPListnerAcceptSocket;
+
+        private byte[] byteSendMsg = new byte[1024];
+        private bool bSocketEnd = false;
+        private int intPortNum = 3333;
+        public int intCientID = 0;
+        private string strLog = "";
+        private string strErr = "";
+        private string strCurTime = "";
+        private string strClientID = "";
+        private int intSize = 0;
+        public string xValue = "";
+        public int ClientCnt = 0;
+        static public TcpServerSocket svr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            if (ServerSocket != null)
+            {
+                //strErr = string.Format("[서버] : 현재 서버 가동중입니다.!!");
+                //   Add_Log(strErr);
+                return;
+            }
+            ServerThread = new Thread(new ThreadStart(Thread_Server_Run));
+            ServerThread.IsBackground = true;
+            ServerThread.Start();
+        }
+        public void Thread_Server_Run()
+        {
+
+            svr = new TcpServerSocket(true);
+            svr.Bind();
+            svr.Listen();
+
+            svr.AcceptStart();
+           label32.Text = "SERVER OPEN";
+            while (true)
+            {
+                //svr.Update();
+
+                if (svr.rcvList.Count != 0)
+                {
+
+                    for (int i = 0; i < svr.rcvList.Count; i++)
+                    {
+                        string txt = String.Format("{0:X2} ", svr.rcvList[i]);
+                      //  string  strReceiveMsg = Encoding.Default.GetString(bytes, 0, intSize);
+                        richTextBox1.AppendText(txt);
+                     
+                     
+                    }
+                    richTextBox1.AppendText("\r\n");
+                    svr.rcvList.Clear();
+                    richTextBox1.ScrollToCaret();
+
+                }
+                Thread.Sleep(50);
+            }
+
+            /*
+            ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            ServerSocket.Bind(new IPEndPoint(IPAddress.Any, intPortNum));
+            ServerSocket.Listen(9);
+
+            IPHostEntry host = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = host.AddressList[0];
+      
+
+            label32.Text = "SERVER OPEN";
+     
+            button10.Visible = true;
+
+          
+
+            while (bSocketEnd == false)
+            {
+
+
+                try
+                {
+                    //  Socket socClient = (Socket)ServerSocket.Accept();
+                    ClientSocket = (Socket)ServerSocket.Accept();
+
+                    ClientMessage_Process(ClientSocket);
+
+
+
+                    byte[] Sendbuff = new byte[4096];
+
+                    Sendbuff[0] = (byte)Int32.Parse(strClientID);
+                    Sendbuff[1] = 0x00;
+
+
+                    ClientCnt++;
+                    strLog = string.Format("[{0}]?묒냽", strClientID);
+                    //     Add_Log(strLog);
+                    // label8.Text = strLog;
+
+                    if (strClientID == "1")
+                    {
+                        client1_Visible(true);
+                        //  Client1_Icon.Visible = true;
+                        //   label8.Text = strLog;
+                    }
+                    else if (strClientID == "2")
+                    {
+                        client2_Visible(true);
+                        // Client2_Icon.Visible = true;
+                        //    label9.Text = strLog;
+                    }
+                    else if (strClientID == "3")
+                    {
+                        client3_Visible(true);
+                        // Client3_Icon.Visible = true;
+                        //   label10.Text = strLog;
+                    }
+                    else if (strClientID == "4")
+                    {
+                        client4_Visible(true);
+                        // Client4_Icon.Visible = true;
+
+                        //   label11.Text = strLog;
+                    }
+                    else if (strClientID == "0")
+                    {
+                        Sendbuff[0] = 0xFD;
+                    }
+                    else if (strClientID == "@")
+                    {
+                        Sendbuff[0] = 0xFE;
+                    }
+
+                    ClientSocket.BeginSend(Sendbuff, 0, byteSendMsg.Length, SocketFlags.None, new AsyncCallback(CallBack_SendMsg), ClientSocket);
+                }
+                catch (Exception err)
+                {
+
+                    MessageBox.Show(err.Message);
+               
+
+                }
+
+
+            }*/
+        }
+
+        private void spTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+
+            Client_label.Text = "Client : " + svr.ClientCnt.ToString();
+
+
+        }
+
+        private void btnSendMsg_Click(object sender, EventArgs e)  // 데이터 보내기 
+        {
+            svr.TachoStr = txtSendMsg.Text;
+            svr.TachoPacket();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+}
